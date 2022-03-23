1c2833ce7044323612a5f3510b36e5a4b551927f
apgenius1001 <apgenius1001@gmail.com>
2022-03-23 15:49:34 +0100

    Remove_consecutive_elements_string

---
+++ remove_consecutive_elements_string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void removeDuplicates(std::string& s)
+{
+    std::string converted;
+    auto prev = s.front();
+    converted = prev;
+    
+    for(auto it = s.begin(); it != s.end(); ++it) {
+        cout << *it << prev << endl;;
+        if(prev != *it) {
+            converted.push_back(*it);
+        }
+        prev = *it;
+    }
+    std::cout << "converted string: " << converted << std::endl;
+}
+
+int main()
+{
+    std::string S("aabbccdddwaacc");
+    
+    removeDuplicates(S);
+    return 0;
+}
+
